0ad165df97999124063fc0a2545a4b2152ee83a0
Neil Lawrence <lawrennd@gmail.com>
2025-04-27 10:12:25 +0100

    Changes for testing from local docs.

--- tax_document_checker/__init__.py
+++ tax_document_checker/__init__.py
@@ -4,4 +4,6 @@ Tax Document Checker
 A Python utility for checking and validating tax documents based on configurable patterns and frequencies.
 """
 
+from .checker import TaxDocumentChecker
+
 __version__ = "0.1.0" 
